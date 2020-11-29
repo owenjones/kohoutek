35fffd97dd1645939e2c457669821ceb9a9506e3
Owen Jones <owen@owenjones.net>
2020-11-29 18:06:46 +0000

    new team portal

---
+++ site/app/controllers/portal.py
@@ -0,0 +1,78 @@
+import time
+
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask_login import current_user, login_user, logout_user
+
+from app import limiter
+from app.models import Entry, Permission
+from app.utils.auth import needs_team
+
+blueprint = Blueprint("portal", __name__, url_prefix="/portal")
+
+
+@blueprint.route("")
+def index():
+    if current_user.hasPermission(Permission.TEAM):
+        entry = current_user.entry
+        return render_template("portal/index.jinja", entry=entry)
+    else:
+        return redirect(url_for("portal.login"))
+
+
+@blueprint.route("/login")
+def login():
+    return render_template("portal/need-login.jinja")
+
+
+@blueprint.route("login/<int:entry>/<string:code>")
+@limiter.limit("5/minute")
+@limiter.limit("20/hour")
+def verify(entry, code):
+    entry = Entry.query.filter_by(id=entry, verification_code=code).first()
+    if entry:
+        verify = entry.verify(code)
+        if verify:
+            flash(
+                "Thank you for verifying your email, your Kohoutek 2021 entry is now confirmed!",
+                "success",
+            )
+
+        login_user(entry.user)
+        return redirect(url_for("portal.index"))
+
+    else:
+        return redirect(url_for("portal.login"))
+
+
+@blueprint.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("root.index"))
+
+
+@blueprint.route("/resend-link", methods=["GET"])
+def resendLink():
+    return render_template("portal/resend-link.jinja")
+
+
+@blueprint.route("/resend-link", methods=["POST"])
+def resendLinkProcess():
+    e = Entry.query.filter_by(contact_email=request.form.get("email")).first()
+    if e:
+        e.sendConfirmationEmail()
+    else:
+        time.sleep(1)  # crap attempt to avoid a timing attack
+
+    flash(
+        "If an entry with this email exists, the team portal link has been resent",
+        "success",
+    )
+    return render_template("portal/resend-link.jinja")
+
+
+## DEV ROUTES
+@blueprint.route("/test/<int:id>")
+def test(id):
+    e = Entry.query.get(id)
+    e.sendConfirmationEmail()
+    return "OK?"
