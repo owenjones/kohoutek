fb96110716b5c5bdc46d08f2ffe5f731aaaa4ee3
Owen Jones <owen@owenjones.net>
2020-12-13 02:59:22 +0000

    new models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,3 @@
 from .User import Role, Permission, User
 from .Entry import Entry, Organisation, County, District
-from .Order import Order, Item
+from .Order import Item, Order, Payment, PaymentMethod
