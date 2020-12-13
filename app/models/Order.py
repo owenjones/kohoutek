8d56d92096b4b043e35dbe915b527488a72e9a18
Owen Jones <owen@owenjones.net>
2020-12-13 02:58:19 +0000

    new association table for orders/items, and table for payment data

--- app/models/Order.py
+++ app/models/Order.py
@@ -1,11 +1,12 @@
+from enum import Enum
+
 from app import db
 
-orderitems = db.Table(
-    "order_items",
-    db.Column("order.id", db.Integer, db.ForeignKey("order.id"), primary_key=True),
-    db.Column("item_id", db.Integer, db.ForeignKey("item.id"), primary_key=True),
-    db.Column("quantity", db.Integer, default=1, nullable=False),
-)
+
+class OrderItems(db.Model):
+    order_id = db.Column(db.Integer, db.ForeignKey("order.id"), primary_key=True)
+    item_id = db.Column(db.Integer, db.ForeignKey("item.id"), primary_key=True)
+    quantity = db.Column(db.Integer, default=1, nullable=False)
 
 
 class Item(db.Model):
@@ -15,6 +16,10 @@ class Item(db.Model):
     image = db.Column(db.String(100))
     quantity = db.Column(db.Integer, default=0, nullable=False)
     unit_cost = db.Column(db.Float, nullable=False)
+
+
+class OrderStatus(Enum):
+    pending = "pending"
 
 
 class Order(db.Model):
@@ -29,3 +34,37 @@ class Order(db.Model):
         lazy=True,
         backref=db.backref("orders", lazy=True),
     )
+
+    @property
+    def amount(self):
+        pass
+        # return sum([ (i.quantity * i.)])
+
+
+class PaymentMethod(Enum):
+    cheque = "cheque"
+    BACS = "BACS"
+    stripe = "stripe"
+    manual = "manual"
+
+
+class PaymentStatus(Enum):
+    pending = "pending"
+    confirmed = "confirmed"
+    error = "error"
+
+
+class Payment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    order_id = db.Column(db.Integer, db.ForeignKey("order.id"), nullable=False)
+    order = db.relationship(
+        "Order", backref=db.backref("payment", lazy=True, uselist=False)
+    )
+    method = db.Column(db.Enum(PaymentMethod), nullable=False)
+    status = db.Column(
+        db.Enum(PaymentStatus), nullable=False, default=PaymentStatus.pending
+    )
+    amount_gross = db.Column(db.Float, nullable=False)
+    amount_net = db.Column(db.Float, nullable=False)
+    reference = db.Column(db.String, nullable=True)
