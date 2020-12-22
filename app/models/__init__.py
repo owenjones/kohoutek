f0947d51e0c38e2c4e8f7cfd6b2c637b602d7b76
Owen Jones <owen@owenjones.net>
2020-12-22 21:04:48 +0000

    added new models (ItemStock and Postage)

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,5 @@
 from .User import Role, Permission, User
 from .Entry import Entry, Organisation, County, District
-from .Item import Item, OrderItem
-from .Order import Order, OrderStatus
+from .Item import Item, OrderItem, ItemStock
+from .Order import Order, OrderStatus, Postage
 from .Payment import Payment, PaymentMethod, PaymentStatus
