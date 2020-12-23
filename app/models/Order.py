907989895bd9970e9b98aa61cf6738ea1dae643e
Owen Jones <owen@owenjones.net>
2020-12-23 23:33:05 +0000

    added postage info columns; tweaked status enum; add postage method; add item method checks stock now

--- app/models/Order.py
+++ app/models/Order.py
@@ -15,7 +15,6 @@ class Postage(db.Model):
 
 class OrderStatus(Enum):
     incomplete = "incomplete"
-    payment_pending = "payment_pending"
     complete = "complete"
     dispatched = "dispatched"
     cancelled = "cancelled"
@@ -30,10 +29,36 @@ class Order(db.Model):
     entry_id = db.Column(db.Integer, db.ForeignKey("entry.id"), nullable=False)
     entry = db.relationship("Entry", backref=db.backref("orders", lazy=True))
 
-    items = db.relationship("OrderItem", backref=db.backref("order", lazy=True))
+    items = db.relationship(
+        "OrderItem",
+        backref=db.backref("order", lazy=True),
+        cascade="all, delete-orphan",
+    )
 
     postage_id = db.Column(db.Integer, db.ForeignKey("postage.id"), nullable=True)
-    postage = db.relationship("Postage", lazy=True)
+    postage = db.relationship(
+        "Postage",
+        lazy=True,
+    )
+
+    postage_name = db.Column(db.Text, nullable=True)
+    postage_address_1 = db.Column(db.Text, nullable=True)
+    postage_address_2 = db.Column(db.Text, nullable=True)
+    postage_city = db.Column(db.Text, nullable=True)
+    postage_postcode = db.Column(db.Text, nullable=True)
+    postage_country = db.Column(db.Text, nullable=True)
+
+    payment_id = db.Column(db.Integer, db.ForeignKey("payment.id"), nullable=True)
+    payment = db.relationship(
+        "Payment",
+        backref=db.backref(
+            "order",
+            lazy=True,
+            uselist=False,
+        ),
+        single_parent=True,
+        cascade="all, delete-orphan",
+    )
 
     def save(self):
         db.session.add(self)
@@ -44,17 +69,21 @@ class Order(db.Model):
 
     @property
     def subtotal(self):
-        return sum([item.amount for item in self.items])
-
-    @property
-    def total(self):
-        total = self.subtotal
+        # Total for order NOT including payment processing fees
+        total = sum([item.amount for item in self.items])
 
         if self.postage:
             total += self.postage.cost
 
+        return total
+
+    @property
+    def total(self):
+        # Total for order including all fees
+        total = self.subtotal
+
         if self.payment:
-            total += self.payment.fee
+            total += self.payment.fee or 0
 
         return total
 
@@ -65,9 +94,8 @@ class Order(db.Model):
     @property
     def status_name(self):
         map = {
-            OrderStatus.incomplete: "Incomplete",
-            OrderStatus.payment_pending: "Payment Pending",
-            OrderStatus.complete: "Complete",
+            OrderStatus.incomplete: "In Progress",
+            OrderStatus.complete: "Payment Received",
             OrderStatus.dispatched: "Dispatched",
             OrderStatus.cancelled: "Cancelled",
         }
@@ -78,7 +106,6 @@ class Order(db.Model):
     def status_colour(self):
         map = {
             OrderStatus.incomplete: "warning",
-            OrderStatus.payment_pending: "warning",
             OrderStatus.complete: "success",
             OrderStatus.dispatched: "success",
             OrderStatus.cancelled: "danger",
@@ -87,23 +114,31 @@ class Order(db.Model):
         return map[self.status]
 
     def addItem(self, item_id, quantity):
-        stock_item = Item.query.get(item_id)
+        if quantity > 0:
+            stock_item = Item.query.get(item_id)
 
-        if stock_item:
-            if stock_item.hasStock(quantity):
-                item = OrderItem.query.filter_by(
-                    order_id=self.id, item_id=item_id
-                ).first()
+            if stock_item:
+                if stock_item.hasStock(quantity):
+                    item = OrderItem.query.filter_by(
+                        order_id=self.id, item_id=item_id
+                    ).first()
 
-                if item:
-                    item.quantity += quantity
+                    if item:
+                        item.quantity += quantity
 
-                else:
-                    item = OrderItem(item_id=item_id, quantity=quantity)
-                    self.items.append(item)
+                    else:
+                        item = OrderItem(item_id=item_id, quantity=quantity)
+                        self.items.append(item)
 
-            else:
+                    return True
+
                 return ItemStock.out_of_stock
 
+            else:
+                return False
+
         else:
-            return None
+            return True  # technically true
+
+    def addPostage(self, id):
+        self.postage_id = id
