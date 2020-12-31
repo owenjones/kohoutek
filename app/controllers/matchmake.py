a2c1857d37d51fe308cf03bbed42a1ea4a6edf48
Owen Jones <owen@owenjones.net>
2020-12-31 02:33:26 +0000

    added sending messages to other matches

--- app/controllers/matchmake.py
+++ app/controllers/matchmake.py
@@ -13,8 +13,9 @@ from flask import (
 from flask_login import current_user
 
 from app import db
-from app.models import Matchmake
+from app.models import Matchmake, Message
 from app.utils.auth import needs_team
+from app.utils.mail import sendmail
 
 blueprint = Blueprint("matchmake", __name__, url_prefix="/portal")
 
@@ -104,11 +105,68 @@ def detailsRemove():
 @blueprint.route("/match/contact/<int:id>")
 @needs_team
 def contact(id):
-    # need team to have allowed contacting first
-    return render_template("portal/matchmake/contact.jinja")
+    match = Matchmake.query.get_or_404(id)
+    if not match.contact:
+        flash(
+            "This entry has requested not to be contacted through the team portal",
+            "warning",
+        )
+        return redirect(url_for("matchmake.index"))
+
+    if match == current_user.entry.match:
+        flash(
+            "You can't send yourself a message!",
+            "warning",
+        )
+        return redirect(url_for("matchmake.index"))
+
+    return render_template("portal/matchmake/contact.jinja", match=match)
 
 
 @blueprint.route("/match/contact/<int:id>", methods=["POST"])
 @needs_team
 def contactProcess(id):
-    return render_template("portal/matchmake/contact.jinja")
+    match = Matchmake.query.get(request.form.get("id"))
+    if not match.contact:
+        flash(
+            "This entry has requested not to be contacted through the team portal",
+            "warning",
+        )
+        return redirect(url_for("matchmake.index"))
+
+    if "message" not in request.form or request.form.get("message") == "":
+        flash(
+            "You haven't entered a message to send",
+            "warning",
+        )
+        return redirect(url_for("matchmake.contact"), id=match.id)
+
+    message = request.form.get("message")
+
+    save = Message(
+        sent_from_id=current_user.entry.id, sent_to_id=match.entry.id, message=message
+    )
+    db.session.add(save)
+    db.session.commit()
+
+    send = sendmail(
+        match.entry.contact_email,
+        "Kohoutek Matchmaker",
+        "matchmake-contact",
+        entry_name=current_user.entry.name,
+        message=message.split("\n"),
+        reply_link=match.entry.portal_link(
+            "matchmake.contact", id=current_user.entry.id
+        ),
+    )
+
+    if send.status_code == 200:
+        flash(f"Your message has been sent to { match.entry.name }", "success")
+
+    else:
+        flash(
+            f"There was an error sending your message to { match.entry.name }",
+            "warning",
+        )
+
+    return redirect(url_for("matchmake.index"))
