b0c58e9c4b2400bb748695ffe5175e3c27422356
Owen Jones <owen@owenjones.net>
2021-01-02 16:39:06 +0000

    added more tables

--- app/models/Score.py
+++ app/models/Score.py
@@ -1,7 +1,26 @@
 from app import db
 
 
+class Activity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=True)
+
+
 class Score(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    team_id = db.Column(db.Integer, db.ForeignKey("team.id"))
+    team = db.relationship("Team", backref=db.backref("scores"))
+    activity_id = db.Column(db.Integer, db.ForeignKey("activity.id"))
+    score = db.Column(db.Integer, default=0)
+
+
+class Team(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=True)
+
     entry_id = db.Column(db.Integer, db.ForeignKey("entry.id"), nullable=False)
-    entry = db.relationship("Entry", backref=db.backref("scores", lazy=True))
+    entry = db.relationship("Entry", backref=db.backref("teams", lazy=True))
+
+    @property
+    def score(self):
+        return sum([s.score for s in self.scores])
