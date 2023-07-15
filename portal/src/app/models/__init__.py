ad9c4c401fead93e1916784f0bacadb37049e397
Owen Jones <owen@owenjones.net>
2023-07-16 00:48:10 +0100

    removed controllers and models for all unused modules

--- portal/src/app/models/__init__.py
+++ portal/src/app/models/__init__.py
@@ -1,7 +1,2 @@
 from .User import Role, Permission, User
 from .Entry import Entry, Organisation, County, District
-from .Item import Item, OrderItem, ItemStock
-from .Order import Order, OrderStatus, Postage
-from .Payment import Payment, PaymentMethod, PaymentStatus
-from .Matchmake import Matchmake, Message
-from .Score import Activity, EntryActivities, Score, Section, Team
