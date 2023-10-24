ecdd21f4c0dd8a56482e78a79be9d982dddf4f23
Owen Jones <owen@owenjones.net>
2023-10-24 01:08:21 +0100

    test portal route

--- portal/routes/web.php
+++ portal/routes/web.php
@@ -1,20 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RootController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\{PortalController, RootController};
 
 Route::get('/', [RootController::class, 'index'])->name('root.index');
 Route::get('/sign-up', function (Request $request) {
   return Redirect::route('root.index');
 });
 Route::post('/sign-up', [RootController::class, 'receive_signup'])->name('root.sign-up-post');
+
+Route::get('/portal/', [PortalController::class, 'index']);
