bc49026e8086d2388e6fa07761a06be27ff3987f
Owen Jones <owen@owenjones.net>
2023-11-03 22:06:06 +0000

    fix alert vars

--- portal/resources/views/portal/components/alerts.blade.php
+++ portal/resources/views/portal/components/alerts.blade.php
@@ -5,9 +5,7 @@ $alerts = session('alert');
 @if ($alerts)
     <div class="uk-width-1-1">
         @foreach ($alerts as $type => $message)
-            <div class="uk-alert uk-alert-{{ type }}">
-                {{ message | safe }}
-            </div>
+            <div class="uk-alert uk-alert-{{ $type }}">{!! $message !!}</div>
         @endforeach
     </div>
 @endif
