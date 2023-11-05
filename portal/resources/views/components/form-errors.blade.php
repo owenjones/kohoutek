90c899019d911a780886e96fe60026f9ce0f20f8
Owen Jones <owen@owenjones.net>
2023-11-05 01:53:05 +0000

    only show as list if more than one error

--- portal/resources/views/components/form-errors.blade.php
+++ portal/resources/views/components/form-errors.blade.php
@@ -1,9 +1,13 @@
 @if ($errors->any())
-    <div class="uk-alert uk-alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
+  <div class="uk-alert uk-alert-danger">
+    @if (count($errors) > 1)
+      <ul>
+        @foreach ($errors->all() as $error)
+          <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    @else
+      {{ $errors->all()[0] }}
+    @endif
+  </div>
 @endif
