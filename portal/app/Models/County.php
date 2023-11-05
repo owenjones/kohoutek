26e2c1b282fabe40eef301749e1c65a94694ad35
Owen Jones <owen@owenjones.net>
2023-11-05 16:51:50 +0000

    new attribute for organisation

--- portal/app/Models/County.php
+++ portal/app/Models/County.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class County extends Model
 {
@@ -11,10 +12,18 @@ class County extends Model
     'code',
     'name',
   ];
+
   public $timestamps = false;
 
   public function districts(): HasMany
   {
     return $this->hasMany(District::class);
+  }
+
+  protected function type(): Attribute
+  {
+    return Attribute::make(
+      get: fn () => ($this->code == "avon") ? "Scouting" : "Guiding"
+    );
   }
 }
