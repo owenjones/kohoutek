2cb7bd6a4201b83d6514162512048cab1279c1a6
Owen Jones <owen@owenjones.net>
2023-11-05 14:29:26 +0000

    new admin user seeder

--- portal/database/seeders/DatabaseSeeder.php
+++ portal/database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+          AdminUserSeeder::class,
           CountyDistrictSeeder::class,
         ]);
     }
