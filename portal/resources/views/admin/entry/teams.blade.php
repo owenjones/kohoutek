60a0902ce6bd55e28951de6f5bedd5aef4fd8136
Owen Jones <owen@owenjones.net>
2023-11-10 03:19:30 +0000

    list teams

--- portal/resources/views/admin/entry/teams.blade.php
+++ portal/resources/views/admin/entry/teams.blade.php
@@ -2,5 +2,24 @@
 @section('module')
   <div class="uk-card uk-card-default uk-card-body uk-width-1-1">
     <h4 class="uk-card-title">Teams</h4>
+    <table class="uk-table uk-table-resonsive uk-table-middle uk-table-striped">
+      <thead>
+        <th class="uk-table-shrink">Code</th>
+        <th class="uk-table-expand">Name</th>
+        <th></th>
+      </thead>
+      <tbody>
+        @foreach ($entry->teams as $team)
+          <tr>
+            <td>{{ $team->code }}</td>
+            <td>{{ $team->name }}</td>
+            <td>Modify</td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+
+    <h5>Add New Team</h5>
+    <p>Form to add a new team to the entry</p>
   </div>
 @endsection
