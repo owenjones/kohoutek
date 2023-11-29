b289bcb51387524ef6de5bf916143abb8d7968b6
Owen Jones <owen@owenjones.net>
2023-11-29 16:50:03 +0000

    forget alert data, as flash seems to not always do this properly

--- portal/resources/views/components/alerts.blade.php
+++ portal/resources/views/components/alerts.blade.php
@@ -1,11 +1,12 @@
 <?php
 $alerts = session('alert');
+session()->forget('alert');
 ?>
 
 @if ($alerts)
-    <div class="uk-width-1-1">
-        @foreach ($alerts as $type => $message)
-            <div class="uk-alert uk-alert-{{ $type }}">{!! $message !!}</div>
-        @endforeach
-    </div>
+  <div class="uk-width-1-1">
+    @foreach ($alerts as $type => $message)
+      <div class="uk-alert uk-alert-{{ $type }}">{!! $message !!}</div>
+    @endforeach
+  </div>
 @endif
