a99f8b7e0eb99b8e71bcae645dd380cbed0f29e3
Owen Jones <owen@owenjones.net>
2023-11-30 18:22:07 +0000

    lots more info

--- portal/resources/views/root/section/about.blade.php
+++ portal/resources/views/root/section/about.blade.php
@@ -11,6 +11,11 @@
           Scouts society (UOBGAS).
         </p>
         <p>
+          Teams of five will take part in 20 themed bases, scoring points based on how well they attempt
+          these, and how they work together as a team. The team which scores the most challenge points overall will win
+          the <em>Kohoutek Trophy</em>, and the team which worked together best the <em>Teamwork Spoon</em>.
+        </p>
+        <p>
           The 2024 event <strong>Welcome to the Jungle!</strong> will be taking place on
           <strong>Saturday 16<sup>th</sup> March 2024</strong> at
           <strong><a href="https://maps.app.goo.gl/NbBBZDJQMLDfoiVu9" target="_blank" rel="nofollow noreferrer">Camerton
@@ -22,44 +27,39 @@
         <h2>How does it work?</h2>
       </div>
       <div class="uk-width-3-5@l">
-        <h3>Entry Cost</h3>
+        <h3>Registration</h3>
+        <p>
+          Groups/Units wishing to take part need to register using the sign up form above, which will allocate them a
+          team. Each Group/Unit can initially enter one team, we will allow additional teams in the new year on a
+          first-come first-served basis, if spaces allow.
+        </p>
+
+        <h3>Information for leaders</h3>
+        <p>
+          A full leader information pack including timings and the plan for the day will be sent out in February to all
+          teams.
+        </p>
+        <p>
+          In advance of this, some useful information:
+        <ul class="uk-list uk-list-square">
+          <li>
+            The day will run from roughly 10am to 4:30pm, and will be outdoors.
+          <li>
+            Teams will need a leader on the day to be responsible for them (leaders can be responsible for multiple
+            teams).
+          </li>
+          <li>
+            Tea/coffee/biscuits for leaders will be provided!
+          </li>
+        </ul>
+        </p>
+
+        <h3>Entry cost</h3>
         <p>
           Entry to the competition is £{{ settings()->get('payment_fee') }} a team, which includes a badge for each
           participant (and leader).
         </p>
-        <p>
-          Payment is due two weeks after signing up or adding a team, or by 4<sup>th</sup> March 2024, which ever comes
-          first.
-        </p>
 
-        <h3 id="rules">Rules</h3>
-        <p>
-          This is a competition after all!
-        </p>
-        <ol class="uk-list uk-list-decimal uk-text-small">
-          <li>
-            The competition is open to Guides within Bristol and South Gloucestershire, and Somerset North
-            Counties; and Scouts within Avon County.
-          </li>
-          <li>
-            Team members must be aged between 10 and 14.5 at the time of the event.
-          </li>
-          <li>
-            Each team must be accompanied by an adult, who will stay on site and remain responsible for them during the
-            event.
-          </li>
-          <li>
-            Accompanying adults can be responsible for multiple teams, across multiple Groups/Units, as long as any
-            relevant organisation ratios are properly met - this is up to Groups/Units to organise.
-          </li>
-          <li>
-            Teams are not confirmed until team payment is received, this will be confirmed via email.
-          </li>
-          <li>
-            Team cancellations must be made before 1<sup>st</sup> February to receive a refund, cancellations made after
-            this date may receive a partial refund at the organisers discretion.
-          </li>
-        </ol>
         <hr class="uk-divider-icon uk-hidden@l" />
       </div>
       <div class="uk-width-2-5@l">
@@ -71,19 +71,19 @@
           <dt>January 2024</dt>
           <dd>Groups can enter additional teams if space allows</dd>
 
-          <dt>4<sup>th</sup> March 2024</dt>
-          <dd>Registration closes and final payment deadline</dd>
+          <dt>1<sup>st</sup> February 2024</dt>
+          <dd>Registration and team payment deadline</dd>
 
           <dt>16<sup>th</sup> March 2024</dt>
           <dd>Join us for Kohoutek 2024</dd>
         </dl>
-        <hr class="uk-divider-icon uk-hidden@l" />
-        <h3>Badges?</h3>
+
+        <h3 id="rules">Rules</h3>
         <p>
-          Yes, there'll be badges!
+          This is a competition after all!
         </p>
         <p>
-          All young people taking part will receive a badge, along with one leaders' badge per team.
+          <a href="{{ asset('rules.pdf') }}" target="_blank" class="uk-button uk-button-secondary">Kohoutek Rules</a>
         </p>
       </div>
     </div>
