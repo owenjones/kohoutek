b48847d6c7ff3363aa380eb50b00eebdfe11ce14
Owen Jones <owen@owenjones.net>
2023-11-30 01:07:52 +0000

    fleshed out info

--- portal/resources/views/root/section/about.blade.php
+++ portal/resources/views/root/section/about.blade.php
@@ -1,59 +1,86 @@
 <div id="about" class="uk-section uk-section-default uk-height-viewport avoid-sticky-nav">
-    <div class="uk-container uk-container-small">
-        <h1 class="uk-text-center">Kohoutek 2024</h1>
-        <div class="uk-flex uk-flex-stretch" uk-grid>
-            <div>
-                <p>
-                    Kohoutek is a competition and activity day for Scouts in Avon County, and Guides in Bristol and
-                    South
-                    Gloucestershire, and Somerset North counties, organised by students from the University of Bristol
-                    Guides and
-                    Scouts society (UOBGAS).
-                </p>
-                <p>
-                    The 2024 event <strong>Welcome to the Jungle!</strong> will be taking place on
-                    <strong>XXXXXX</strong> at
-                    <strong>XXXXXX</strong>.
-                </p>
-                <hr class="uk-divider-icon uk-hidden@l" />
-            </div>
-            <div class="uk-width-1-1">
-                <h2>How does it work?</h2>
-            </div>
-            <div class="uk-width-3-5@l">
-                <h3 id="rules">Rules</h3>
-                <p>
-                    This is a competition after all!
-                </p>
-                <ol class="uk-list uk-list-decimal uk-text-small">
-                    <li>
-                        The competition is open to Guides within Bristol and South Gloucestershire, and Somerset North
-                        Counties; and Scouts within Avon County.
-                    </li>
-                    <li>
-                        <strong>TODO: dig out old rules from pre-covid events</strong>
-                    </li>
-                </ol>
-                <hr class="uk-divider-icon uk-hidden@l" />
-            </div>
-            <div class="uk-width-2-5@l">
-                <h3>Key Dates</h3>
-                <dl class="uk-description-list uk-description-list-divider">
-                    <dt>December 2023</dt>
-                    <dd>Registration for Kohoutek 2024 opens</dd>
+  <div class="uk-container uk-container-small">
+    <h1 class="uk-text-center">Kohoutek 2024</h1>
+    <div class="uk-flex uk-flex-stretch" uk-grid>
+      <div>
+        <p>
+          Kohoutek is a competition and activity day for Scouts in Avon County, and Guides in Bristol and
+          South
+          Gloucestershire, and Somerset North counties, organised by students from the University of Bristol
+          Guides and
+          Scouts society (UOBGAS).
+        </p>
+        <p>
+          The 2024 event <strong>Welcome to the Jungle!</strong> will be taking place on
+          <strong>Saturday 16<sup>th</sup> March 2024</strong> at
+          <strong><a href="https://maps.app.goo.gl/NbBBZDJQMLDfoiVu9" target="_blank" rel="nofollow noreferrer">Camerton
+              Community Hall</a></strong>.
+        </p>
+        <hr class="uk-divider-icon uk-hidden@l" />
+      </div>
+      <div class="uk-width-1-1">
+        <h2>How does it work?</h2>
+      </div>
+      <div class="uk-width-3-5@l">
+        <h3>Entry Cost</h3>
+        <p>Entry to the competition is £35 a team, which includes a badge for each participant (and leader).</p>
+        <p>Payment is due two weeks after signing up or adding a team, or by 4<sup>th</sup> March 2024, which ever comes
+          first.</p>
 
-                    <dt>16<sup>th</sup> March 2024</dt>
-                    <dd>Kohoutek 2024</dd>
-                </dl>
-                <hr class="uk-divider-icon uk-hidden@l" />
-                <h3>Badges?</h3>
-                <p>
-                    Yes, there'll be badges!
-                </p>
-                <p>
-                    All young people taking part will receive a badge, along with one leaders' badge per team.
-                </p>
-            </div>
-        </div>
+        <h3 id="rules">Rules</h3>
+        <p>
+          This is a competition after all!
+        </p>
+        <ol class="uk-list uk-list-decimal uk-text-small">
+          <li>
+            The competition is open to Guides within Bristol and South Gloucestershire, and Somerset North
+            Counties; and Scouts within Avon County.
+          </li>
+          <li>
+            Team members must be aged between 10 and 14.5 at the time of the event.
+          </li>
+          <li>
+            Each team must be accompanied by an adult, who will stay on site and remain responsible for them during the
+            event.
+          </li>
+          <li>
+            Accompanying adults can be responsible for multiple teams, across multiple Groups/Units, as long as any
+            relevant organisation ratios are properly met - this is up to Groups/Units to organise.
+          </li>
+          <li>
+            Teams are not confirmed until team payment is received, this will be confirmed via email.
+          </li>
+          <li>
+            Team cancellations must be made before 1<sup>st</sup> February to receive a refund, cancellations made after
+            this date may receive a partial refund at the organisers discretion.
+          </li>
+        </ol>
+        <hr class="uk-divider-icon uk-hidden@l" />
+      </div>
+      <div class="uk-width-2-5@l">
+        <h3>Key Dates</h3>
+        <dl class="uk-description-list uk-description-list-divider">
+          <dt>1<sup>st</sup> December 2023</dt>
+          <dd>Registration for Kohoutek 2024 opens</dd>
+
+          <dt>January 2024</dt>
+          <dd>Groups can enter additional teams if space allows</dd>
+
+          <dt>4<sup>th</sup> March 2024</dt>
+          <dd>Registration closes and final payment deadline</dd>
+
+          <dt>16<sup>th</sup> March 2024</dt>
+          <dd>Join us for Kohoutek 2024</dd>
+        </dl>
+        <hr class="uk-divider-icon uk-hidden@l" />
+        <h3>Badges?</h3>
+        <p>
+          Yes, there'll be badges!
+        </p>
+        <p>
+          All young people taking part will receive a badge, along with one leaders' badge per team.
+        </p>
+      </div>
     </div>
+  </div>
 </div>
