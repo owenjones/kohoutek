b141cd9827afaaca206f837bdee1d7c73e2de767
Owen Jones <owen@owenjones.net>
2023-11-30 21:39:01 +0000

    copy updates

--- portal/resources/views/root/section/history.blade.php
+++ portal/resources/views/root/section/history.blade.php
@@ -1,34 +1,34 @@
 <div id="history" class="uk-section uk-section-secondary uk-height-viewport avoid-sticky-nav">
-    <div class="uk-container uk-container-small">
-        <h1 class="uk-text-center uk-margin-remove-bottom">Kohoutek History</h1>
-        <p>
-            <em>Kohoutek, Kahoutek, Cahoutek, Cohoutek - one event, many spellings!</em>
-        </p>
-        <p>
-            Kohoutek was originally held in 1973 by a group of University of Bristol Students, for local Scout Troops
-            and Guide Units, based off a similar event held by Scouts in Cheshire. It was named after <a
-                href="https://www.history.nasa.gov/SP-404/ch4.htm" target="_blank" rel="nofollow noopener">Comet
-                Kohoutek</a>, which was first sighted in March of that year. Over the years themes for the event have
-            ranged from Jurassic Park (1997) to Wild West (2020)!
-        </p>
-        <p>
-            The event aims to provide a fun environment for Scouts and Guides to improve their teamwork and problem
-            solving skills. Teams of about five meet at Woodhouse Park to complete a series of themed activity bases
-            over the course of the day, competing for the coveted Kohoutek Trophy.
-        </p>
-        <p>
-            There was a short hiatus in the event when the University of Bristol Guides and Scouts (UOBGAS) society
-            closed in the early 2000s, but the rebirth of the society saw the event being brought back in 2017. 2019 saw
-            us celebrating the 35th Kohoutek event, with Kohoutek hosting more than 300 young people!
-        </p>
-        <h4>Coverage</h4>
-        <p>2019 - Epigram (University of Bristol Students' newspaper): <a
-                href="https://epigram.org.uk/2019/03/18/kohoutek-2019/" target="_blank" rel="nofollow noopener"><em>Student
-                    Scouts and Guides organise mammoth event for local children named after the Kohoutek comet</em></a>.
-        </p>
+  <div class="uk-container uk-container-small">
+    <h1 class="uk-text-center uk-margin-remove-bottom">Kohoutek History</h1>
+    <p>
+      <em>Kohoutek, Kahoutek, Cahoutek, Cohoutek - one event, many spellings!</em>
+    </p>
+    <p>
+      Kohoutek was originally held in 1973 by a group of University of Bristol Students, for local Scout Troops
+      and Guide Units, based off a similar event held by Scouts in Cheshire. It was named after <a
+        href="https://www.history.nasa.gov/SP-404/ch4.htm" target="_blank" rel="nofollow noopener">Comet
+        Kohoutek</a>, which was first sighted in March of that year. Over the years themes for the event have
+      ranged from Jurassic Park (1997) to Wild West (2020)!
+    </p>
+    <p>
+      The event aims to provide a fun environment for Scouts and Guides to improve their teamwork and problem
+      solving skills. Teams of five meet to complete a series of themed activity bases
+      over the course of the day, competing for the coveted Kohoutek Trophy.
+    </p>
+    <p>
+      There was a short hiatus in the event when the University of Bristol Guides and Scouts (UOBGAS) society
+      closed in the early 2000s, but the rebirth of the society saw the event being brought back in 2017. 2019 saw
+      us celebrating the 35th Kohoutek event, with Kohoutek hosting more than 300 young people!
+    </p>
+    <h4>Coverage</h4>
+    <p>2019 - Epigram (University of Bristol Students' newspaper): <a
+        href="https://epigram.org.uk/2019/03/18/kohoutek-2019/" target="_blank" rel="nofollow noopener"><em>Student
+          Scouts and Guides organise mammoth event for local children named after the Kohoutek comet</em></a>.
+    </p>
 
-        <p>2020 - Avon Scout Radio: <a
-                href="https://www.facebook.com/media/set/?vanity=avonscoutradio&set=a.1765261640277612" target="_blank"
-                rel="nofollow noopener">Event photos</a> and live broadcast.</p>
-    </div>
+    <p>2020 - Avon Scout Radio: <a
+        href="https://www.facebook.com/media/set/?vanity=avonscoutradio&set=a.1765261640277612" target="_blank"
+        rel="nofollow noopener">Event photos</a> and live broadcast.</p>
+  </div>
 </div>
