e339f0c63b0892dd09ab150e3e246abdeea0fb54
Owen Jones <owen@owenjones.net>
2024-02-03 16:23:11 +0000

    remove unused imports

--- portal/app/Http/Controllers/Admin/PaymentController.php
+++ portal/app/Http/Controllers/Admin/PaymentController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Str;
-use Illuminate\Support\Facades\DB;
 
 use App\Http\Controllers\Controller;
 use App\Models\{Payment, Team};
